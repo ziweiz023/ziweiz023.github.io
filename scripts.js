41e665721b5b6634d3bf23138797f3b5e50f515b
ziweiz023 <zz4980@nyu.edu>
2025-04-19 14:16:54 -0400

    clean up

--- scripts.js
+++ scripts.js
@@ -1,1 +1,30 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//   const exploreBtn = document.getElementById("explore-map");
+//   const cviSection = document.getElementById("CVI");
 
+//   if (exploreBtn && cviSection) {
+//     exploreBtn.addEventListener("mouseenter", () => {
+//       cviSection.style.transition = "transform 0.5s ease";
+//       cviSection.style.transform = "scale(1.03)";
+//     });
+
+//     exploreBtn.addEventListener("mouseleave", () => {
+//       cviSection.style.transform = "scale(1)";
+//     });
+//   }
+// });
+
+document.addEventListener("DOMContentLoaded", () => {
+    const exploreBtn = document.getElementById("explore-map");
+    const cviSection = document.getElementById("CVI");
+  
+    if (exploreBtn && cviSection) {
+      exploreBtn.addEventListener("mouseenter", () => {
+        cviSection.classList.add("zoomed");
+      });
+  
+      exploreBtn.addEventListener("mouseleave", () => {
+        cviSection.classList.remove("zoomed");
+      });
+    }
+  });
