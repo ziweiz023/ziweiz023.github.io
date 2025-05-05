4f442ff47b3af8546728d14566a364ba29794765
ziweiz023 <zz4980@nyu.edu>
2025-05-05 17:58:52 -0400

    final commit before submission

--- scripts.js
+++ scripts.js
@@ -1,18 +1,3 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//   const exploreBtn = document.getElementById("explore-map");
-//   const cviSection = document.getElementById("CVI");
-
-//   if (exploreBtn && cviSection) {
-//     exploreBtn.addEventListener("mouseenter", () => {
-//       cviSection.style.transition = "transform 0.5s ease";
-//       cviSection.style.transform = "scale(1.03)";
-//     });
-
-//     exploreBtn.addEventListener("mouseleave", () => {
-//       cviSection.style.transform = "scale(1)";
-//     });
-//   }
-// });
 
 document.addEventListener("DOMContentLoaded", () => {
     const exploreBtn = document.getElementById("explore-map");
@@ -32,6 +17,21 @@ document.addEventListener("DOMContentLoaded", () => {
 
   document.addEventListener("DOMContentLoaded", () => {
     const exploreBtn = document.getElementById("read-report");
+    const cviSection = document.getElementById("report");
+  
+    if (exploreBtn && cviSection) {
+      exploreBtn.addEventListener("mouseenter", () => {
+        cviSection.classList.add("zoomed");
+      });
+  
+      exploreBtn.addEventListener("mouseleave", () => {
+        cviSection.classList.remove("zoomed");
+      });
+    }
+  });
+
+  document.addEventListener("DOMContentLoaded", () => {
+    const exploreBtn = document.getElementById("poster");
     const cviSection = document.getElementById("report");
   
     if (exploreBtn && cviSection) {
